495510f504281fe407ae42bc816683cf13bf884a
Brian <bstuart777@gmail.com>
2022-09-10 04:31:21 -0400

    Added code to stop the top bar from showing

--- app/src/main/java/com/example/myw_app/MainActivity.java
+++ app/src/main/java/com/example/myw_app/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.myw_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println();
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide(); //hides top bar
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.edit_profile);
     }
 }
