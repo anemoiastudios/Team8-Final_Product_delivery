b69552eee4c8aa4210172595d8ee4fbc5b19aa8a
yourecuteuwu <ckosmoahoy@gmail.com>
2022-09-25 15:43:51 -0400

    Added R and I page, fixed bug with crashing emulator

---
+++ app/src/main/java/com/example/myw_app/RandIActivity.java
@@ -0,0 +1,14 @@
+package com.example.myw_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RandIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rand_iactivity);
+    }
+}
