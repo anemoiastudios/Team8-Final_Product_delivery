c79c176475866b0c0fd9994f40ecdbf77eab5a76
yourecuteuwu <ckosmoahoy@gmail.com>
2022-09-27 19:36:08 -0400

    Added the basic UI for the card views for the R and I page

--- app/src/main/java/com/example/myw_app/RandIActivity.java
+++ app/src/main/java/com/example/myw_app/RandIActivity.java
@@ -1,14 +1,19 @@
 package com.example.myw_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class RandIActivity extends AppCompatActivity {
 
+    private RecyclerView rAndIRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rand_iactivity);
+
+        rAndIRecyclerView = findViewById(R.id.rAndIRecyclerView);
     }
 }
