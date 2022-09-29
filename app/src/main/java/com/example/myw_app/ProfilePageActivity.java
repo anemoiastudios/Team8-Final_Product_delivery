203c43636762f90a52653ac479c11e0e747845ad
yourecuteuwu <ckosmoahoy@gmail.com>
2022-09-28 23:42:23 -0400

    Finished the RecyclerView for the profile page

--- app/src/main/java/com/example/myw_app/ProfilePageActivity.java
+++ app/src/main/java/com/example/myw_app/ProfilePageActivity.java
@@ -1,9 +1,13 @@
 package com.example.myw_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class ProfilePageActivity extends AppCompatActivity {
 
@@ -16,6 +20,19 @@ public class ProfilePageActivity extends AppCompatActivity {
 
         journalRecyclerView = findViewById(R.id.journalRecyclerView);
 
+        ArrayList<Journal> journals = new ArrayList<>();
+
+        journals.add(new Journal("This is the first journal", "This is a journal entry"));
+        journals.add(new Journal("This is the second journal", "1234213"));
+        journals.add(new Journal("This is the third journal", "asdua"));
+        journals.add(new Journal("This is the fourth journal", "asdadasd"));
+        journals.add(new Journal("This is the fifth journal", "asdasod"));
+
+        JournalRecyclerViewAdapter adapter = new JournalRecyclerViewAdapter(this);
+        adapter.setJournals(journals);
+
+        journalRecyclerView.setAdapter(adapter);
+        journalRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
     }
 }
