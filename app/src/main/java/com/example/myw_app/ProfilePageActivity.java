8e61ca6378230be86f102a36a0aaf62dc565a1ba
yourecuteuwu <ckosmoahoy@gmail.com>
2022-09-28 22:35:06 -0400

    Finished the top half of the profile page

--- app/src/main/java/com/example/myw_app/ProfilePageActivity.java
+++ app/src/main/java/com/example/myw_app/ProfilePageActivity.java
@@ -1,14 +1,21 @@
 package com.example.myw_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class ProfilePageActivity extends AppCompatActivity {
 
+    private RecyclerView journalRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        journalRecyclerView = findViewById(R.id.journalRecyclerView);
+
+
     }
 }
