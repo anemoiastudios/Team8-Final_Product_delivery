d6998e23085560a22c92c85ede4683b9f8785efc
Brian <bstuart777@gmail.com>
2022-10-06 01:59:07 -0400

    Deleted Un needed code

--- app/src/main/java/com/example/myw_app/MainActivity.java
+++ app/src/main/java/com/example/myw_app/MainActivity.java
@@ -17,13 +17,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //getSupportActionBar().hide(); //hides top bar
-        System.out.println("Hello");
+
         super.onCreate(savedInstanceState);
-
-
 
 
         setContentView(R.layout.edit_profile);
