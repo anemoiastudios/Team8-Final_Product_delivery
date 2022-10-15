cee129531b3bd8c080328f20b7e5eccd23006070
Brian <bstuart777@gmail.com>
2022-10-15 01:05:58 -0400

    Fixed a bug that showed default button color

--- app/src/main/java/com/example/myw_app/appointment_page_activity.java
+++ app/src/main/java/com/example/myw_app/appointment_page_activity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class appointment_page_activity extends AppCompatActivity {
     private Button upcomingButton, pastButton, otherButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -16,19 +17,22 @@ public class appointment_page_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.appointment_page);
 
-        pastButton=findViewById(R.id.pastButton);
-        otherButton=findViewById(R.id.otherButton);
-        upcomingButton=findViewById(R.id.upcomingButton);
+        pastButton = findViewById(R.id.pastButton);
+        otherButton = findViewById(R.id.otherButton);
+        upcomingButton = findViewById(R.id.upcomingButton);
 
         pastButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                pastButton.setBackgroundColor(Color.parseColor("#DFDFDF"));
+                pastButton.setBackgroundColor(Color.parseColor("#027989"));
                 pastButton.setTextColor(Color.WHITE);
 
                 upcomingButton.setTextColor(Color.parseColor("#027989"));
+                upcomingButton.setBackgroundColor(Color.parseColor("#DFDFDF"));
+
                 otherButton.setTextColor(Color.parseColor("#027989"));
+                otherButton.setBackgroundColor(Color.parseColor("#DFDFDF"));
 
             }
         });
@@ -37,12 +41,14 @@ public class appointment_page_activity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                otherButton.setBackgroundColor(Color.parseColor("#DFDFDF"));
+                otherButton.setBackgroundColor(Color.parseColor("#027989"));
                 otherButton.setTextColor(Color.WHITE);
 
-                pastButton.setTextColor(Color.parseColor("#027989"));
                 upcomingButton.setTextColor(Color.parseColor("#027989"));
+                upcomingButton.setBackgroundColor(Color.parseColor("#DFDFDF"));
 
+                pastButton.setTextColor(Color.parseColor("#027989"));
+                pastButton.setBackgroundColor(Color.parseColor("#DFDFDF"));
             }
         });
 
@@ -50,12 +56,15 @@ public class appointment_page_activity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                upcomingButton.setBackgroundColor(Color.parseColor("#DFDFDF"));
+                upcomingButton.setBackgroundColor(Color.parseColor("#027989"));
                 upcomingButton.setTextColor(Color.WHITE);
 
                 pastButton.setTextColor(Color.parseColor("#027989"));
-                otherButton.setTextColor(Color.parseColor("#027989"));
+                pastButton.setBackgroundColor(Color.parseColor("#DFDFDF"));
 
+                otherButton.setTextColor(Color.parseColor("#027989"));
+                otherButton.setBackgroundColor(Color.parseColor("#DFDFDF"));
             }
         });
+    }
 }
